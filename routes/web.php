954f89df1e5f1bc8c599977c065b126e2771e627
Artur Serkeli <serkeli2006@gmail.com>
2024-07-14 16:13:01 +0300

    Added Controller

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,7 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return 'Roma is available';
 });
+
+Route::get('/my_page', 'MyPlaceController@index');
